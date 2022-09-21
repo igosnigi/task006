9b6bd59a0a24efff9377a5c69abe192ca8e3aac6
igor <igor.kot.snig@yandex.ru>
2022-09-21 10:22:34 +0500

    третье задание

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Clear();
+
+Console.WriteLine("введите число: ");
+string input = Console.ReadLine();
+int number = int.Parse(input);
+
+if (number%2 == 0)
+{
+    Console.WriteLine("да");
+}
+else{
+    Console.WriteLine("нет");
+}
